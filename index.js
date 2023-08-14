0b208129b48d59278d957885b806ef4c229be1d0
Jesus Calderin <jesus.c@servicepad.com>
2023-08-14 11:53:25 -0400

    fix

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+
+// This line is important to ensure your app listens to the PORT env var
+const port = process.env.PORT ?? 8080;
 
 app.get("/", (req, res) => {
   res.send({ message: "Hello World!" });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
